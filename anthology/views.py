d9d8d3050931a9da059e5a4a368cb32f6fc2aca2
emmanueluwa <emmanuel@fulodev.com>
2022-09-20 05:01:19 +0100

    adding option to share post via email

--- anthology/views.py
+++ anthology/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 # Create your views here.
 
 def post_list(request):
@@ -27,3 +30,34 @@ def post_detail(request, year, month, day, post):
             publish__day=day
         )
     return render(request, 'anthology/post/detail.html', {'post': post})
+
+
+"""
+class based view for list view
+"""
+class PostListView(ListView):
+    queryset = Post.published.all()
+    #for the queryset results
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'anthology/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        #FORM was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{clean_data['name']} recomends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{clean_data['name']}\'s comments: {clean_data['comments']}"
+            send_mail(subject, message, 'fulphrone@gmail.com', [clean_data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'anthology/post/share.html', {'post':post, 'form':form, 'sent': sent})
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #template debug
 TEMPLATES[0]["OPTIONS"]["debug"] = True
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('PETS_EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('PETS_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('PETS_EMAIL_HOST_PASSWORD')
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
