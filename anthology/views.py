d0b187bac04edd96c3115ea474aa2e5637a8b610
emmanueluwa <emmanuel@fulodev.com>
2022-09-21 21:12:52 +0100

    adding stemming and ranking to results

--- anthology/views.py
+++ anthology/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.http import require_POST
 from taggit.models import Tag
 #count aggregation function of django orm
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 
 # Create your views here.
@@ -111,6 +111,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
-    
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
+
     return render(request, 'anthology/post/search.html', {'form': form, 'query': query, 'results': results})
