bec146636101151bd2e840d11874178ed9b61230
emmanueluwa <emmanuel@fulodev.com>
2022-09-21 20:24:43 +0100

    changing style and adding ability to search

--- anthology/views.py
+++ anthology/views.py
@@ -8,6 +8,9 @@ from django.views.decorators.http import require_POST
 from taggit.models import Tag
 #count aggregation function of django orm
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
+
 # Create your views here.
 
 def post_list(request, tag_slug=None):
@@ -99,3 +102,15 @@ def post_comment(request, post_id):
         comment.save()
     return render(request, 'anthology/post/comment.html', {'post':post, 'form': form, 'comment': comment})
 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        #get method sent so resulting url includes query parameter and is easy to share
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+    
+    return render(request, 'anthology/post/search.html', {'form': form, 'query': query, 'results': results})
