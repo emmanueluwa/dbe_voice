6f3109def297697f6e402aa0d52490da3f9fcba1
emmanueluwa <emmanuel@fulodev.com>
2022-09-21 10:39:57 +0100

    adding custom template filter allowing markdown to be used for posts

--- anthology/templatetags/anthology_tags.py
+++ anthology/templatetags/anthology_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 #tag retrieving total posts published
 
@@ -19,3 +21,8 @@ def show_latest_posts(count=5):
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+#template filter
+@register.filter(name='markdown_')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
