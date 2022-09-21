d919484bdf124d10e1b34d340e2734426cd3f30c
emmanueluwa <emmanuel@fulodev.com>
2022-09-21 22:04:30 +0100

    adding full-text search engine of postgres

--- anthology/views.py
+++ anthology/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.http import require_POST
 from taggit.models import Tag
 #count aggregation function of django orm
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from .forms import EmailPostForm, CommentForm, SearchForm
 
 # Create your views here.
@@ -111,9 +111,6 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + \
-                            SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
-            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query),).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request, 'anthology/post/search.html', {'form': form, 'query': query, 'results': results})
